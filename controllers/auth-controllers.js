076f04a6a1d21028cbc678a97457a983d4c0db84
Akshay <krishnarathore2302@gmail.com>
2025-06-25 11:19:56 +0530

    Update auth-controllers.js

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,5 +1,6 @@
 // Note: MongoDB User model removed - implement custom authentication
 // let User = require("../models/users-database");
+const passport = require("passport");
 
 const showRegisterForm = (req,res) => {
     // register form
@@ -27,10 +28,25 @@ const showLoginForm = (req,res) => {
 }
 
 const loginUser = (req, res) => {
-    // TODO: Implement custom login logic without MongoDB
-    console.log("user logged in")
-    req.flash('success', 'Login Successful')
-    res.redirect('/profile');
+    // Use Passport authentication
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            req.flash('error', 'Login failed');
+            return res.redirect('/login');
+        }
+        if (!user) {
+            req.flash('error', 'Invalid credentials');
+            return res.redirect('/login');
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                req.flash('error', 'Login failed');
+                return res.redirect('/login');
+            }
+            req.flash('success', 'Login Successful');
+            res.redirect('/profile');
+        });
+    })(req, res);
 }
 
 const logoutUser = (req, res) => {
